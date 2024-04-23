1148631f98241fe8ea9406b23a1889f9ade1f631
Polina170608 <151750038+Polina170608@users.noreply.github.com>
2024-04-23 18:07:31 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,20 +11,19 @@ app = Flask(__name__)
 db_session.global_init("db/users.db")
 db_sess = db_session.create_session()
 name_pict = None
-pictures = {'1': None,
-            '2': None,
-            '3': None}
+pictures = {'1': [],
+            '2': [],
+            '3': []}
 for picture in db_sess.query(Picture).all():
-    if picture[5] == 1:
-        pictures['1'][picture[1]] = [picture[3], picture[4]]
-        pictures['1'][picture[1]]['key'] = [picture[2]]
-        pictures['1'][picture[1]]['name'] = [picture[1]]
-    elif picture[5] == 2:
-        pictures['2'][picture[1]] = [picture[3], picture[4]]
-        pictures['2'][picture[1]]['key'] = [picture[2]]
-    elif picture[5] == 3:
-        pictures['3'][picture[1]] = [picture[3], picture[4]]
-        pictures['3'][picture[1]]['key'] = [picture[2]]
+    if picture.level == 1:
+        pictures['1'].append({'lon': picture.long, 'lat': picture.lat,
+                              'key': picture.key, 'name': picture.name})
+    elif picture.level == 2:
+        pictures['2'].append({'lon': picture.long, 'lat': picture.lat,
+                              'key': picture.key, 'name': picture.name})
+    elif picture.level == 3:
+        pictures['3'].append({'lon': picture.long, 'lat': picture.lat,
+                              'key': picture.key, 'name': picture.name})
 
 logging.basicConfig(level=logging.INFO)
 
@@ -55,13 +54,13 @@ def handle_dialog(res, req):
             'first_name': None,
             'key_word': None,
             'game_started': False,
-            'level': None,
+            'level': '1',
             'objects': [],
-            'score': 0
+            'score': 0,
+            'add_object': False
         }
         return
     if sessionStorage[user_id]['first_name'] is None:
-        print('ok')
         key_word = None
         first_name = None
         if len(req['request']['original_utterance'].split()) == 2:
@@ -99,9 +98,8 @@ def handle_dialog(res, req):
         if not sessionStorage[user_id]['game_started']:
             if 'да' in req['request']['nlu']['tokens']:
                 sessionStorage[user_id]['game_started'] = True
-                sessionStorage[user_id]['level'] = 1
+                sessionStorage[user_id]['level'] = '1'
                 res['response']['text'] = 'Хорошо, угадай первый объект'
-
                 game(res, req)
             elif 'нет' in req['request']['nlu']['tokens']:
                 res['response']['text'] = 'Хорошо, увидимся позже'
@@ -119,37 +117,66 @@ def handle_dialog(res, req):
                     }
                 ]
         else:
-            if req['request']['nlu']['tokens'].lower() == name_pict:
-                sessionStorage[user_id]['objects'].append(name_pict)
-                sessionStorage[user_id]['score'] += 1
-                res['response']['text'] = 'Вы угадали! Хотите продолжить игру?'
-                name_pict = None
-                return
+            if name_pict:
+                if name_pict.lower() in req['request']['original_utterance'].lower():
+                    sessionStorage[user_id]['objects'].append(name_pict)
+                    sessionStorage[user_id]['score'] += 1
+                    res['response']['text'] = 'Вы угадали! Хотите продолжить игру?'
+                    name_pict = None
+                    return
+                else:
+                    res['response']['text'] = f'К сожалению, вы не угадали. Это {name_pict}. Хотите продолжить игру?'
+                    name_pict = None
+                    sessionStorage[user_id]['objects'].append(name_pict)
+                    return
             else:
-                res['response']['text'] = f'К сожалению, вы не угадали это {name_pict}. Хотите продолжить игру?'
-                name_pict = None
-                sessionStorage[user_id]['objects'].append(name_pict)
-                return
-            if 'да' in req['request']['nlu']['tokens']:
-                game(res, req)
-                return
-            else:
-                res['response']['text'] = f'Хорошо, приятно было с вами сыграть. Ваш счёт ' \
-                                          f'{sessionStorage[user_id]["score"]}'
-                return
+                if ('да' in req['request']['original_utterance']) and int(sessionStorage[user_id]['level']) < 4:
+                    res['response']['text'] = 'Итак, мы продолжаем'
+                    game(res, req)
+                    return
+                elif ('да' in req['request']['original_utterance']) and int(sessionStorage[user_id]['level']) > 3:
+                    sessionStorage[user_id]['add_object'] = True
+                    res['response'][
+                        'text'] = 'Хорошо, вам нужно отправить название объекта (вместо пробела используй _ ), ' \
+                                  'его долготу и широту (без запятых), ' \
+                                  'а также предполагаемый уровень сложности (1, 2, 3)'
+                    return
+                elif sessionStorage[user_id]['add_object']:
+                    if len(req['request']['original_utterance'].split()) == 4:
+                        name = ' '.join(req['request']['original_utterance'].split('_')[0])
+                        lat = req['request']['original_utterance'].split()[1]
+                        long = req['request']['original_utterance'].split()[2]
+                        level = req['request']['original_utterance'].split()[3]
+                        pict = Picture()
+                        pict.name = name
+                        pict.long = long
+                        pict.lat = lat
+                        pict.level = level
+                        db_sess = db_session.create_session()
+                        db_sess.add(pict)
+                        db_sess.commit()
+                        geocode(lat, long, user_id)
+                        res['response']['text'] = f'Хорошо, приятно было с вами сыграть. Ваш счёт ' \
+                                                  f'{sessionStorage[user_id]["score"]}'
+                        return
+                    else:
+                        res['response']['text'] = 'Необходимо указать 4 слова'
+                        return
+                else:
+                    res['response']['text'] = f'Хорошо, приятно было с вами сыграть. Ваш счёт ' \
+                                              f'{sessionStorage[user_id]["score"]}'
+                    return
 
 
-def geocode(lat, long):
-    url = 'https://static-maps.yandex.ru/v1'
-    map_request = f'http://static-maps.yandex.ru/1.x/?ll={lat},{long}&spn=30,30&l=sat'
+def geocode(lat, long, user_id):
+    map_request = f'http://static-maps.yandex.ru/1.x/?ll={lat},{long}&0.05,0.05&l=sat'
     response = requests.get(map_request)
-
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
-    map_file = "img/map.png"
+    map_file = f"img/map{user_id}.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
 
@@ -158,41 +185,24 @@ def game(res, req):
     global name_pict
     user_id = req['session']['user_id']
     if len(sessionStorage[user_id]['objects']) == len(pictures[sessionStorage[user_id]['level']]):
-        sessionStorage[user_id]['level'] += 1
+        sessionStorage[user_id]['level'] = str(int(sessionStorage[user_id]['level']) + 1)
         sessionStorage[user_id]['objects'] = []
-        if sessionStorage[user_id]['level'] > len(pictures):
-            res['response'][
-                'text'] = f'Вы прошли игру, ваш счёт {sessionStorage[user_id]["score"]}.' \
-                          f' Хотите добавить свои данные к нам в базу?'
-            if 'да' in req['request']['nlu']['tokens']:
-                res['response']['text'] = 'Хорошо, вам нужно отправить название объекта, его долготу и широту'
-                if len(req['request']['original_utterance'].split()) == 3:
-                    name = req['request']['original_utterance'].split()[0]
-                    lat = req['request']['original_utterance'].split()[1]
-                    long = req['request']['original_utterance'].split()[3]
-                    picture.name = name
-                    picture.long = long
-                    picture.lat = lat
-                    db_sess.add(picture)
-                    geocode(lat, long)
-                else:
-                    res['response']['text'] = 'Необходимо указать 3 слова'
-                    return
-            else:
-                res['response']['text'] = f'Хорошо, приятно было с вами сыграть.'
-                return
-    key_pict = random.choice(pictures[sessionStorage[user_id]['level']]['key'])
+        if int(sessionStorage[user_id]['level']) > len(pictures):
+            res['response']['text'] = f'Вы прошли игру. Хотите добавить свои данные к нам в базу?'
+            return
+        res['response']['text'] = f'Вы угадали все объекты данного уровня, поэтому переходите на следующий. ' \
+                                  f'Ваш счёт {sessionStorage[user_id]["score"]}.'
+    key_pict = random.choice(pictures[str(sessionStorage[user_id]['level'])])
+    name_pict = key_pict['name']
     while name_pict in sessionStorage[user_id]['objects']:
-        key_pict = random.choice(pictures[sessionStorage[user_id]['level']]['key'])
-    name_pict = pictures['1'][picture[1]]['name']
+        key_pict = random.choice(pictures[str(sessionStorage[user_id]['level'])])
+        name_pict = key_pict['name']
+    key_pict = key_pict['key']
+    print(name_pict)
     res['response']['card'] = {}
     res['response']['card']['type'] = 'BigImage'
     res['response']['card']['title'] = 'Угадай, что это за объект'
     res['response']['card']['image_id'] = key_pict
-
-
-# TODO ПОДУМАТЬ над переносом в верхнюю функцию
-# увелечиние уровню сложности относительно количества указанных картинок или завершение игры
 
 
 def get_first_name(req):
